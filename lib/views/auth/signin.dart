c1563c03b6ed79c1070978410dd4b2b501807748
Solly M <smotsoane@globalasp.com>
2020-12-29 12:17:51 +0200

    Auth Views
    Sectioned Signup and Signin views.
    Added nav between them.

--- lib/views/auth/signin.dart
+++ lib/views/auth/signin.dart
@@ -1,10 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:starter_with_firestore/views/auth/signup.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        color: Colors.pink[200],
+        child: Column(
+          children: [
+            _LogoSection(),
+            _SignInFormSection(),
+            _SocialButtonsSection(),
+            Spacer(),
+            _SignUpSection(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SignUpSection extends StatelessWidget {
+  const _SignUpSection({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            bottom: 50,
+            top: 30,
+          ),
+          child: GestureDetector(
+              onTap: () => Get.to(SignUpView()),
+              child: Text('Don\'t have an account? Sign up here.')),
+        ),
+      ),
+    );
+  }
+}
+
+class _SocialButtonsSection extends StatelessWidget {
+  const _SocialButtonsSection({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      height: 200,
+    );
+  }
+}
+
+class _SignInFormSection extends StatelessWidget {
+  const _SignInFormSection({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.purple,
+      height: 200,
+    );
+  }
+}
+
+class _LogoSection extends StatelessWidget {
+  const _LogoSection({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      height: 200,
+    );
   }
 }
--- lib/views/auth/signup.dart
+++ lib/views/auth/signup.dart
@@ -1,10 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:starter_with_firestore/views/auth/signin.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        color: Colors.pink[200],
+        child: Column(
+          children: [
+            _LogoSection(),
+            _SignUpFormSection(),
+            _SocialButtonsSection(),
+            Spacer(),
+            _SignInSection(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SignInSection extends StatelessWidget {
+  const _SignInSection({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            bottom: 50,
+            top: 30,
+          ),
+          child: GestureDetector(
+              onTap: () => Get.to(SignInView()),
+              child: Text('Have an account already? Sign in here.')),
+        ),
+      ),
+    );
+  }
+}
+
+class _SocialButtonsSection extends StatelessWidget {
+  const _SocialButtonsSection({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      height: 200,
+    );
+  }
+}
+
+class _SignUpFormSection extends StatelessWidget {
+  const _SignUpFormSection({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.purple,
+      height: 200,
+    );
+  }
+}
+
+class _LogoSection extends StatelessWidget {
+  const _LogoSection({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      height: 200,
+    );
   }
 }
